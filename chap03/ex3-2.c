45832b751723ae83dd28c4ab2205a7ec42881c70
momo9 <liangchenchi@gmail.com>
2014-05-01 16:18:51 +0800

    add ex3-2, escape_rvt()

--- chap03/ex3-2.c
+++ chap03/ex3-2.c
@@ -22,11 +22,34 @@ static void escape(char s[], char t[]) {
   s[is] = t[it];
 }
 
+static void escape_rvt(char s[], char t[]) {
+  int it, is;
+  for (it = 0, is = 0; t[it]; ++it) {
+    switch (t[it]) {
+      case '\\':
+        ++it;
+        switch (t[it]) {
+          case 'n':
+            s[is++] = '\n';
+            break;
+          case 't':
+            s[is++] = '\t';
+            break;
+        }
+        break;
+      default:
+        s[is++] = t[it];
+        break;
+    }
+  }
+}
+
 main(int argc, char *argv[]) {
   char *t = argv[1];
   char s[N];
 
   puts(t);
-  escape(s, t);
+  //escape(s, t);
+  escape_rvt(s, t);
   puts(s);
 }
