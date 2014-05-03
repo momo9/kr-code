40bd4814d27f9c581c8a967211fa80541813301d
momo9 <liangchenchi@gmail.com>
2014-05-03 10:28:09 +0800

    add ex4-3

---
+++ chap04/stack.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "stack.h"
+
+#define STACK_SIZE    64
+
+static double a[STACK_SIZE];
+static int top;
+
+double pop() {
+  if (top > 0) {
+    return a[--top];
+  } else {
+    puts("The stack is empty.");
+  }
+}
+
+void push(double data) {
+  if (top == STACK_SIZE) {
+    puts("The stack is full.");
+  } else {
+    a[top++] = data;
+  }
+}
+
+void print_stack() {
+  int i;
+  for (i = 0; i < top; ++i) {
+    printf("%g ", a[i]);
+  }
+  puts("");
+}
+
+#undef STACK_SIZE
---
+++ chap04/stack.h
@@ -0,0 +1,8 @@
+#ifndef STACK_H
+#define STACK_H
+
+double pop();
+void push(double data);
+void print_stack();
+
+#endif
