02ac2955037be3ac7cdbcfef28c99c0d0ede7f1a
momo9 <liangchenchi@gmail.com>
2014-05-04 12:02:13 +0800

    add ex4-13

---
+++ chap04/ex4-13.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_SIZE        64
+
+static void rvrs(char s[], int beg, int end) {
+  if (beg <= end) {
+    int temp;
+    temp = s[beg];
+    s[beg++] = s[end];
+    s[end--] = temp;
+    rvrs(s, beg, end);
+  }
+}
+
+main(int argc, char *argv[]) {
+  char s[MAX_SIZE];
+  while (gets(s)) {
+    rvrs(s, 0, strlen(s) - 1);
+    puts(s);
+  }
+}
+
+#undef MAX_SIZE
