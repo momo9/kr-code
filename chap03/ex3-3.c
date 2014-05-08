199d5a90dbcf3202a8b51954cd65021a38d02983
momo9 <liangchenchi@gmail.com>
2014-05-08 18:20:00 +0800

    add commonts on chapter 3, and modify ex3-3

--- chap03/ex3-3.c
+++ chap03/ex3-3.c
@@ -4,6 +4,7 @@
 
 #define N       64
 
+// can be isdigit(), isupper(), islower() and so on
 typedef int (*pfun_type)(int);
 
 static void exp_one(char s1[], int *pis1, int lens1,
@@ -12,17 +13,20 @@ static void exp_one(char s1[], int *pis1, int lens1,
   int end;
   end = *pis1 + 2;
 
-  // not a group or not succesive
-  if (end > lens1 || !pf) {
+  // out of range || not a group  || not succesive
+  if (end > lens1 || !pf(s1[end]) || s1[*pis1 + 1] != '-') {
+    // copy
     for (; s1[*pis1] && *pis1 <= end; ++(*pis1)) {
       s2[(*pis2)++] = s1[*pis1];
     }
+    // give an end
     if (s1[*pis1] == 0) {
       s2[(*pis2)++] = 0;
     } 
     return;
   }
 
+  // expand
   char c;
   for (c = s1[*pis1]; c <= s1[end]; ++c) {
     s2[(*pis2)++] = c;
@@ -41,6 +45,7 @@ static void expand(char s1[], char s2[]) {
       exp_one(s1, &is1, len, s2, &is2, isupper);
     } else if (islower(s1[is1])) {
       exp_one(s1, &is1, len, s2, &is2, islower);
+    // copy
     } else {
       s2[is2++] = s1[is1++];
     }
--- chap03/shellsort.c
+++ chap03/shellsort.c
@@ -3,8 +3,11 @@
 #define N       8
 
 static int a[N] = {2, 7, 3, 1, 5, 8, 4, 6};
+
+// a special case of shell sort (i == 1), like bubble sort
 static void shellsort(int a[]) {
   int i, j;
+  // after each iteration, a[0...i] is sorted
   for (i = 1; i < N; ++i) {
     for (j = i - 1; j >= 0 && a[j] > a[j + 1]; j--) {
       int temp;
