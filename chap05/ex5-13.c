d28460fbdc55c5e74e2cce787fab87b1798ebb97
momo9 <liangchenchi@gmail.com>
2014-05-12 15:08:07 +0800

    add comments on ex5-13

--- chap05/ex5-13.c
+++ chap05/ex5-13.c
@@ -6,6 +6,7 @@
 
 main(int argc, char *argv[]) {
   int n = 10;
+  // get n
   if (argc > 2) {
     puts("argument error.");
     exit(-1);
@@ -17,6 +18,8 @@ main(int argc, char *argv[]) {
   int nline, i;
   nline = readlines(lines, MAX_LINE);
   
+  // when n > nline (the number of lines read), just print nline lines
+  // when n <= nline, print last n lines
   puts("start output");
   for (i = (n < nline) ? nline - n : 0; i < nline; ++i) {
     puts(lines[i]);
